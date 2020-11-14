1ab64e94ba017c23223de2e7a344223c1ba76c02
Vaibhav Singh <40722806+Mrvab@users.noreply.github.com>
2020-11-14 12:54:34 +0530

    rem comments

--- loadmodel.py
+++ loadmodel.py
@@ -11,9 +11,7 @@ from keras.preprocessing.image import load_img
 import matplotlib.pyplot as plt
 from keras.preprocessing import image
 import pickle
-#from keras.preprocessing.image import ImageDataGenerator
-#need a model to load weight
-#
+
 loaded_model.load_weights('checkpoints/weights-improvement-17-0.80.hdf5')
 filename='dogandcat.sav'
 loaded_model = pickle.load(open(filename, 'rb'))
@@ -21,16 +19,8 @@ test_image = load_img('dataset/check/cat/1.jpg',target_size=(64,64))
 test_image = image.img_to_array(test_image)
 test_image = np.expand_dims(test_image, axis = 0)
 
-#test_datagen = ImageDataGenerator(rescale = 1./255)
-
-#test_image =test_datagen.flow_from_directory('dataset/check',
- #                                           target_size = (64, 64),
-  #                                          batch_size = 16,
-   #                                         class_mode = 'binary')
 loaded_model.summary()
 
-#print(test_image[0][0][1])
-#img = test_image[0][0][2].reshape((-1, 64, 64, 3))
 plt.imshow(test_image)
 plt.title("none")
 plt.show()
